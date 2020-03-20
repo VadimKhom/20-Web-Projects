645f1c134322fa2df0a65d17e43e0463335d56ca
Vadim Khomenkov <maestrik007@yandex.ru>
2020-03-20 23:35:37 +0300

    function showError was created

--- form-validator/script.js
+++ form-validator/script.js
@@ -3,3 +3,12 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('passwrod');
 const password2 = document.getElementById('password2');
+
+// Show input error message
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+form.addEventListener('submit', function(e) {})
