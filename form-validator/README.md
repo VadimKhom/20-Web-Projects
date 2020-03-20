3cfea259fd0c39959d7cc782093a69d2311a96f2
Vadim Khomenkov <maestrik007@yandex.ru>
2020-03-20 23:17:21 +0300

    find elements

--- form-validator/README.md
+++
@@ -1,2 +0,0 @@
-20+ Web Projects With Vanilla JavaScript.
-Mini projects built with HTML5, CSS & JavaScript. No frameworks or libraries
---
+++ form-validator/script.js
@@ -0,0 +1,5 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('passwrod');
+const password2 = document.getElementById('password2');
