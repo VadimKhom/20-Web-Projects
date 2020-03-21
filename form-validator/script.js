52f9a90aaff878254cc9920d08db2f063613f582
Vadim Khomenkov <maestrik007@yandex.ru>
2020-03-21 23:51:33 +0300

    eventlistener wasa added

--- form-validator/script.js
+++ form-validator/script.js
@@ -1,29 +1,47 @@
-const form = document.getElementById('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const password = document.getElementById('passwrod');
-const password2 = document.getElementById('password2');
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("passwrod");
+const password2 = document.getElementById("password2");
 
 // Show input error message
 function showError(input, message) {
     const formControl = input.parentElement;
-    formControl.className = 'form-control error';
-    const small = formControl.querySelector('small');
+    formControl.className = "form-control error";
+    const small = formControl.querySelector("small");
     small.innerText = message;
 }
 
 // Show success outline
 function showSuccess(input) {
     const formcontrol = input.parentElement;
-    formControl.className = 'form-control succes';
+    formControl.className = "form-control succes";
 }
 
 // Event listeners
-form.addEventListener('submit', function(e) {
+form.addEventListener("submit", function(e) {
     e.preventDefault();
 
-    if (username.value === '') {
-        showError(username, 'Username is required');
+    if (username.value === "") {
+        showError(username, "Username is required");
+    } else {
+        showSuccess(username);
+    }
+
+    if (email.value === "") {
+        showError(username, "Email is required");
+    } else {
+        showSuccess(username);
+    }
+
+    if (password.value === "") {
+        showError(username, "Password is required");
+    } else {
+        showSuccess(username);
+    }
+
+    if (password2.value === "") {
+        showError(username, "Password 2 is required");
     } else {
         showSuccess(username);
     }
