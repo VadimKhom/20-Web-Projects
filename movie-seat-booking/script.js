493a06da5718635a37ef0b26852d26c4216156ae
Vadim Khomenkov <maestrik007@yandex.ru>
2020-04-08 23:31:24 +0300

    populate

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -3,3 +3,7 @@ const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
+
+populateUI();
+
+let ticketPrice = +movieSelect.value;
