a35f203a0f8c0b4e8e8f08eaf6bbee69472029be
Vadim Khomenkov <maestrik007@yandex.ru>
2020-05-27 20:03:21 +0300

    function getRandomUser

--- DOM Array Methods  forEach, map, filter, sort, reduce/script.js
+++ DOM Array Methods  forEach, map, filter, sort, reduce/script.js
@@ -4,3 +4,25 @@ const doubleBtn = document.getElementById("double");
 const showMillionairesBtn = document.getElementById("show-millionaires");
 const sortBtn = document.getElementById("sort");
 const calculateWealthBtn = document.getElementById("calculate-wealth");
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+//Fetch random user and add money
+
+async function getRandomUser() {
+    const res = await fetch("http://randomuser.me/api");
+    const data = await res.json();
+
+    const user = data.results[0];
+
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random() * 1000000),
+    };
+
+    console.log(newUser);
+}
