a82b7ec915024bd3d09bb618bfd0e553961205b2
Vadim Khomenkov <maestrik007@yandex.ru>
2020-08-02 17:28:05 +0300

    const figureParts was added

--- Hangman Game DOM, SVG, Events/script.js
+++ Hangman Game DOM, SVG, Events/script.js
@@ -4,3 +4,5 @@ const playAgainBtn = document.getElementById("play-again");
 const popup = document.getElementById("popup-container");
 const notification = document.getElementById("notification-container");
 const finalMessage = document.getElementById("final-message");
+
+const figureParts = document.querySelectorAll("figure-part");
